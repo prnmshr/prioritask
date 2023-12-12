14d7adfe6a1ccfffb408c9045f951d71ec84ea69
prnmshr <h1101211065@student.untan.ac.id>
2023-12-12 08:02:30 +0700

    read data with filtering task for home page

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TaskController extends Controller
 {
-    //
+    public function home() {
+        $task=Task::all();
+        return view('site.home', ['task'=>$task]);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'keyword' => 'required',
+            'detail' => 'required',
+            'date' => 'required',
+            'quadrant' => 'required'
+        ]);
+
+        $task = new Task();
+        $task->keyword = $request->keyword;
+        $task->detail = $request->detail;
+        $task->date = $request->date;
+        $task->quadrant = $request->quadrant;
+        $task->save();
+
+        return redirect()->route('home');
+    }
 }
--- resources/views/layouts/partials/modal.blade.php
+++ resources/views/layouts/partials/modal.blade.php
@@ -6,34 +6,39 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form>
+                <form action="{{ route('store') }}" method="POST">
+                    @csrf
                     <div class="mb-3">
-                        <label for="task-keyword" class="col-form-label"></label>
-                        <input type="text" class="form-control" id="task-keyword" placeholder="Task keyword">
+                        <label for="keyword" class="col-form-label"></label>
+                        <input type="text" class="form-control" id="keyword" name="keyword"
+                            placeholder="Task keyword">
                     </div>
                     <div class="mb-3">
-                        <label for="task-detail" class="col-form-label"></label>
-                        <input type="text" class="form-control" id="task-detail" placeholder="Task detail">
+                        <label for="detail" class="col-form-label"></label>
+                        <input type="text" class="form-control" id="detail" name="detail"
+                            placeholder="Task detail">
                     </div>
                     <div class="mb-3">
-                        <label for="due-date" class="col-form-label"></label>
-                        <input type="date" class="form-control" id="due-date" placeholder="Due date">
+                        <label for="date" class="col-form-label"></label>
+                        <input type="date" class="form-control" id="date" name="date" placeholder="Due date">
                     </div>
                     <div class="mb-3">
-                        <label for="recipient-name" class="col-form-label"></label>
-                        <select type="text" class="form-control" id="recipient-name">
-                            <option value="Importan & urgent">Importan & urgent</option>
-                            <option value="Importan less urgent">Importan less urgent</option>
+                        <label for="quadrant" class="col-form-label"></label>
+                        <select class="form-control" id="quadrant" name="quadrant">
+                            <option value="Important & urgent">Important & urgent</option>
+                            <option value="Important less urgent">Important less urgent</option>
                             <option value="Urgent less important">Urgent less important</option>
                             <option value="Not important nor urgent">Not important nor urgent</option>
                         </select>
                     </div>
+
+                    <div class="modal-footer modal-right pb-4">
+                        <button type="submit" class="btn btn-green">Create</button>
+                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                    </div>
                 </form>
             </div>
-            <div class="modal-footer modal-right">
-                <button type="button" class="btn btn-green">Create</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-            </div>
+
         </div>
     </div>
 </div>
